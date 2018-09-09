f0a6694e8586be6de494d1407ad5cb1106b9c081
Joey Muselman <jhm15b@my.fsu.edu>
2018-09-09 14:26:19 -0400

    first commit

--- project1.c
+++ project1.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main( int argc, char *argv[] )  
-	{
-	
-	char *myUSER = getenv("USER");	//declaring variables need for comandline
-	char *myPWD = getenv("PWD");
-	char *myMACHINE = getenv("MACHINE");
-	//check to make sure they all got valid data
-	if (myUSER != NULL && myPWD != NULL && myMACHINE != NULL)	
-		printf("%s@%s :: %s ->\n", myUSER, myMACHINE, myPWD);
-	else 
-		printf("get env returned null");
+int main( int argc, char *argv[] )
+        {
 
-	}
+        char *myUSER = getenv("USER");  //declaring variables need for comandline
+        char *myPWD = getenv("PWD");
+        char *myMACHINE = getenv("MACHINE");
+        //check to make sure they all got valid data
+        if (myUSER != NULL && myPWD != NULL && myMACHINE != NULL)
+                printf("%s@%s :: %s ->\n", myUSER, myMACHINE, myPWD);
+        else
+                printf("get env returned null");
+
+        }
